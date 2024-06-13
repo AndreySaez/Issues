becf541735dfe10566c88f6fdc578f9502a12556
AndreySaez <theleji.andrey@gmail.com>
2024-06-13 19:42:38 +0400

    Добавил прослойку State

--- app/src/main/java/com/example/issues/data/api/LoginInterceptor.kt
+++ app/src/main/java/com/example/issues/data/api/LoginInterceptor.kt
@@ -6,7 +6,7 @@ import okhttp3.Response
 class LoginInterceptor: Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val original = chain.request().newBuilder()
-            .addHeader("Authorization", "Bearer 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")
+            .addHeader("Authorization", "Bearer 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")
             .build()
         return chain.proceed(original)
     }
